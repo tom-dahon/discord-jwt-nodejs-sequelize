b6431309dc6a186ffb9574a97ceb0d5a62bfb21b
Tom Dahon <tomdahon@gmail.com>
2023-01-12 15:05:23 +0100

    Division Back/Front, login terminé

--- server.js
+++ server.js
@@ -35,9 +35,8 @@ app.get("/", (req, res) => {
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
-require('./app/routes/product.routes')(app);
+require('./app/routes/back/auth.routes')(app);
+require('./app/routes/back/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
