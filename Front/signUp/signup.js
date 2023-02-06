c48e666af7bc1e1e2ee35a74538d6cbe4403b119
Tom Dahon <tomdahon@gmail.com>
2023-02-06 16:42:04 +0100

    Création d'un module cookies.js pour importer les méthodes dans les fichiers où on en a besoin

--- Front/signUp/signup.js
+++ Front/signUp/signup.js
@@ -1,6 +1,34 @@
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+import { setCookie } from '../cookies.js';
+
+const form = document.querySelector("form");
+const email = document.querySelector("#username");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const form_error = document.querySelector("#form_error");
+
+form.addEventListener("submit", signUp);
+
+async function signUp(e) {
+    e.preventDefault();
+    const data = { "email": email.value, "username": username.value, "password": password.value };
+    var headers = new Headers();
+    headers.append("Content-Type","application/json");
+    var requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+      };
+      
+    let res = await fetch('/api/auth/signup',requestOptions)
+        .catch(err =>{
+          console.log(err);
+        });
+        if(res.status == 200) {
+          let infos = await res.json();
+        setCookie('token', infos.accessToken, 1);
+        window.location.replace('../home/index.html');
+        } else {
+          form_error.classList.replace('d-none','d-block');
+        }
+        
 }
